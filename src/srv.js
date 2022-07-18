d48738bf723a3004b29f14c9fd79f5df8055f8e1
Sapronov Sergey <1262708@mail.ru>
2022-07-18 19:08:45 +0300

    modified:   package.json 	deleted:    src/config/index.css 	deleted:    src/index.js 	deleted:    src/layout/main/main.css 	deleted:    src/layout/main/main.js 	deleted:    src/layout/main/main.tmpl.js 	deleted:    src/pages/500/500.js 	renamed:    src/index.css -> src/pages/index/index.css 	renamed:    src/layout/main/index.js -> src/pages/index/index.js 	renamed:    src/index.jssx -> src/pages/index/index.jssx 	modified:   src/srv.js

--- src/srv.js
+++ src/srv.js
@@ -3,15 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
-  res.status(404).type('.html').sendFile('index.html', { root: __dirname + '/../dist' });
+  res.status(200).type('.html').sendFile('index.html', { root: __dirname + '/../dist/index/' });
 });
-app.get('/500', function (req, res) {
-  res.status(500).type('.html').sendFile('500.html', { root: __dirname + '/../dist' });
+app.get('/:page', function (req, res) {
+  res.status(200).sendFile(req.params.page, { root: __dirname + '/../dist/' });
 });
 app.get('*', function (req, res) {
-  res.status(404).type('.html').sendFile('404.html', { root: __dirname + '/../dist' });
+  res.status(404).type('.html').sendFile('404.html', { root: __dirname + '/../dist/404' });
 });
-
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).type('.html').sendFile('500.html', { root: __dirname + '/../dist/500' });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
