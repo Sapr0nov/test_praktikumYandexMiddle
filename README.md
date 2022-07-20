ab554c41ba054b3e3d8efb1af9a966479a3ca70f
Sergey S <1262708@mail.ru>
2022-07-20 11:08:11 +0300

    readme

--- README.md
+++ README.md
@@ -11,3 +11,29 @@
 ### 
 ### cd node_modules/@parsel/transformer-templator/
 ### npm install
+
+
+Project from course Yandex - middle Front end developer
+It include part of web application - client for chat app. 
+
+## Getting Started 🚀
+
+* [Figma] (https://www.figma.com/file/ovjYpFJqUreYoOcBK0ixb8/Messanger?node-id=0%3A1) 
+* [DEMO] (https://sprint1.stacksite.ru)
+
+## Using 
+
+**Parcel** **Node JS** **Express** **PosHTML** **PostCSS**
+
+## Commands 💬
+
+* npm start - lounch server
+* npm run clean - clear cache
+* npm run build - compile file for production
+* npm run dev - compile file for development
+
+
+## Documentation
+
+Read the docs at https://parceljs.org/docs/.
+
--- src/srv.js
+++ src/srv.js
@@ -2,19 +2,29 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+app.use('/', express.static(__dirname + '/../dist'));
+
+/** Routing */
 app.get('/', function (req, res) {
   res.status(200).type('.html').sendFile('index.html', { root: __dirname + '/../dist/index/' });
 });
+
 app.get('/:page', function (req, res) {
   res.status(200).sendFile(req.params.page, { root: __dirname + '/../dist/' });
 });
+
+/* 404 error */
 app.get('*', function (req, res) {
   res.status(404).type('.html').sendFile('404.html', { root: __dirname + '/../dist/404' });
 });
+
+/** error server */
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).type('.html').sendFile('500.html', { root: __dirname + '/../dist/500' });
 });
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
