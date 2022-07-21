3a7cf9d6195c1db720e3947e3f2f1356be53edf2
Sergey S <1262708@mail.ru>
2022-07-21 10:54:25 +0300

    final version

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,16 +4,22 @@ document.addEventListener("DOMContentLoaded", function(e) {
     const cookie = new Cookies();
     const auth = document.querySelector("#auth-popup");
     const reg = document.querySelector("#reg-popup");
+
     const profileBtn = document.querySelector(".chat-profile");
     const settingBtn = document.querySelector(".chat-header__setting");
     const authBtn = document.querySelector(".form-auth__btn");
-    const authLing = document.querySelector(".form-auth__link");
     const regBtn = document.querySelector(".form-reg__btn");
+    const closeSetBtn = document.querySelector(".close-settings");
+    const changeDataBtn = document.querySelector(".change-data");
+    const changePswdBtn = document.querySelector(".change-pswd");
+
+    const authLing = document.querySelector(".form-auth__link");
     const regLing = document.querySelector(".form-reg__link");
     const messageHistory = document.querySelector(".message-history");
     const messagesList = document.querySelector("#messages-list");
     const chatList = document.querySelector(".chat-list");
     const userSettings = document.querySelector("#user-settings");
+    const settingForm = document.querySelector(".form-set");
     
     document.querySelectorAll(".message").forEach( (el, index) => {
         if (index % 3) { el.classList.remove("self"); }
@@ -33,36 +39,48 @@ document.addEventListener("DOMContentLoaded", function(e) {
     profileBtn.addEventListener("click", e => {
         e.preventDefault();
         userSettings.classList.remove("hide");
-        console.log('Open profile');
     });
     settingBtn.addEventListener("click", e => {
         e.preventDefault();
         userSettings.classList.remove("hide");
-        console.log('Open settings');
+    });
+    closeSetBtn.addEventListener("click", e => {
+        e.preventDefault();
+        userSettings.classList.add("hide");
     });
     authBtn.addEventListener("click", e => {
         e.preventDefault();
         cookie.setCookie('logined','true');
         checkAccess(userSettings, messagesList, chatList, auth, reg)
-        console.log('auth checking');
     });
     regBtn.addEventListener("click", e => {
         e.preventDefault();
         cookie.setCookie('logined','true');
         checkAccess(userSettings, messagesList, chatList, auth, reg)
-        console.log('reg checking');
     });
+    changeDataBtn.addEventListener("click", e => {
+        e.preventDefault();
+        if ( settingForm.classList.contains("locked") ) {
+            settingForm.classList.remove("locked");
+            changeDataBtn.textContent = "Сохранить";            
+        }else{            
+            settingForm.classList.add("locked");
+            changeDataBtn.textContent = "Изменить данные";
+        }
+    })
+    changePswdBtn.addEventListener("click", e => {
+        e.preventDefault();
+        console.log('popup window');
+    })
     regLing.addEventListener("click", e => {
         e.preventDefault();
         auth.classList.add("hide");
         reg.classList.remove("hide");
-        console.log('reg show');
     });
     authLing.addEventListener("click", e => {
         e.preventDefault();
         auth.classList.remove("hide");
         reg.classList.add("hide");
-        console.log('auth show');
     });
 
     checkAccess(userSettings, messagesList, chatList, auth, reg);
