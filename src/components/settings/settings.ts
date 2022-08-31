bb2d492fe55eb66326c40adcc143d6e8b69e05ba
Sergey S <1262708@mail.ru>
2022-09-01 00:04:33 +0300

    new file:   .eslintignore 	new file:   .eslintrc.json 	modified:   .mocharc.json 	modified:   .prettierrc.json 	new file:   .stylelintrc.json 	modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/aside_chat_preview/aside_chat_preview.css 	modified:   src/components/aside_chat_preview/aside_chat_preview.hbs 	modified:   src/components/aside_chat_preview/aside_chat_preview.ts 	modified:   src/components/chat/chat.css 	modified:   src/components/chat/chat.hbs 	modified:   src/components/chat/chat.ts 	modified:   src/components/glitch_effect/glitch_effect.css 	modified:   src/components/settings/settings.css 	modified:   src/components/settings/settings.ts 	modified:   src/components/side_bar/side_bar.css 	modified:   src/components/side_bar/side_bar.hbs 	modified:   src/components/side_bar/side_bar.ts 	modified:   src/handlebars.d.ts 	modified:   src/index.html 	modified:   src/modules/ApiAction.ts 	modified:   src/modules/Block.ts 	modified:   src/modules/Chat.ts 	modified:   src/modules/Cookie.ts 	modified:   src/modules/EventBus.ts 	modified:   src/modules/Fetch.ts 	modified:   src/modules/Route.ts 	modified:   src/modules/Router.ts 	modified:   src/modules/User.ts 	modified:   src/modules/Validate.ts 	deleted:    src/modules/color.css 	deleted:    src/modules/form.css 	modified:   src/modules/route.spec.ts 	modified:   src/modules/validate.spec.ts 	modified:   src/pages/404/404.css 	modified:   src/pages/404/404.hbs 	modified:   src/pages/404/404.ts 	modified:   src/pages/500/500.css 	modified:   src/pages/500/500.hbs 	modified:   src/pages/500/500.ts 	modified:   src/pages/form_auth/form_auth.hbs 	modified:   src/pages/form_auth/form_auth.ts 	modified:   src/pages/form_registration/form_registration.hbs 	modified:   src/pages/form_registration/form_registration.ts 	modified:   src/pages/index/index.css 	modified:   src/pages/index/index.hbs 	modified:   src/pages/index/index.ts 	modified:   src/srv.js 	modified:   src/srv.spec.ts 	new file:   static/color.css 	modified:   static/data.json 	new file:   static/form.css 	modified:   test/babel-register.js 	modified:   test/index.spec.ts 	modified:   tsconfig.json

--- src/components/settings/settings.ts
+++ src/components/settings/settings.ts
@@ -6,21 +6,21 @@ import image from "../../../static/placeholder.png";
 import { user as User } from "../../modules/User";
 
 export class Settings extends Block {
-    render() {
-        let image_link:string = image;
-        if (User.avatar) {
-            image_link = 'https://ya-praktikum.tech/api/v2/resources' + User.avatar;
-        }
-        let outLine:string = '';
-        outLine = Handlebars.compile(hbs_settings) ({
-            linkback: "/messenger",
-            name: User.first_name,
-            login: User.login,
-            email: User.email,
-            surname: User.second_name,
-            phone: User.phone,
-            avatar: image_link
-        })
-        return outLine;    
+  render() {
+    let image_link: string = image;
+    if (User.avatar) {
+      image_link = "https://ya-praktikum.tech/api/v2/resources" + User.avatar;
     }
+    let outLine: string = "";
+    outLine = Handlebars.compile(hbs_settings)({
+      linkback: "/messenger",
+      name: User.first_name,
+      login: User.login,
+      email: User.email,
+      surname: User.second_name,
+      phone: User.phone,
+      avatar: image_link,
+    });
+    return outLine;
+  }
 }
--- src/handlebars.d.ts
+++ src/handlebars.d.ts
@@ -1,5 +1,5 @@
-declare module '*.hbs';
-declare module '*.css';
-declare module '*.ts';
-declare module '*.json';
-declare module '*.png';
+declare module "*.hbs";
+declare module "*.css";
+declare module "*.ts";
+declare module "*.json";
+declare module "*.png";
